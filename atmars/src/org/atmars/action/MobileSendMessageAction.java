5f0c34efd9773229bde0040502527c87701327d6
cloudzfy <cloudzfy@gmail.com>
2012-07-12 16:15:57 +0000

    Add Mobile API function to the project.

--- atmars/src/org/atmars/action/MobileSendMessageAction.java
+++ atmars/src/org/atmars/action/MobileSendMessageAction.java
@@ -1,8 +1,9 @@
 package org.atmars.action;
 
+import java.util.concurrent.TimeUnit;
 import javax.persistence.criteria.CriteriaBuilder.In;
-
 import org.atmars.dao.Message;
+import org.atmars.utils.*;
 
 public class MobileSendMessageAction extends BaseAction{
 	private static final long serialVersionUID = 1L;
@@ -19,10 +20,16 @@ public class MobileSendMessageAction extends BaseAction{
 			message = mService.sendMessage(userId, text, null, null, forwordId);
 			message.MakeAllSetNull();
 			message.getUser().setPassword(null);
+			message.setTimeDescription(TimeUtils.getTimeDelay(message.getTime()));
+			message.setText(ConvertPostUtils.replaceFace(message.getText()));
+			message.setText(ConvertPostUtils.replaceAtMarkToHTML(message.getText()));
 			if(message.getOriginal()!=null)
 			{
 				message.getOriginal().MakeAllSetNull();
 				message.getOriginal().getUser().setPassword(null);
+				message.getOriginal().setText(ConvertPostUtils.replaceFace(message.getOriginal().getText()));
+				message.getOriginal().setText(ConvertPostUtils.replaceAtMarkToHTML(message.getOriginal().getText()));
+				message.getOriginal().setTimeDescription(TimeUtils.getTimeDelay(message.getOriginal().getTime()));
 			}
 		}
 		return "success";
